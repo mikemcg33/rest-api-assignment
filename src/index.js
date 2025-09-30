3ff2f1a7d170782a16d74bdb8cf5d5a51a0d084f
Michael McGillycuddy <mkm@njit.edu>
2025-09-30 18:28:25 -0400

    Finished REST API implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -8,10 +9,53 @@ app.use(express.json());
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
+const users = [];
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post('/users',(req, res) => {
+    const { name, email } = req.body;
+    if( !name || !email){
+        return res.status(400).send("400 Bad Request");
+    }
+    const id = uuidv4();
+    const user = { id, name, email };
+    users.push(user);
+    return res.status(201).json(user);
 });
+
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = users.find(unique => unique.id == id );
+    if(!user){
+        return res.status(404).send("404 Not Found");
+    }
+    return res.status(200).json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+    const { name, email } = req.body;
+    if( !name || !email){
+        return res.status(400).send("400 Bad Request");
+    }
+    const { id } = req.params;
+    const user = users.find(unique => unique.id == id );
+    if(!user){
+        return res.status(404).send("404 Not Found");
+    }
+    const userIndex = users.findIndex(unique => unique.id == id);
+    users[userIndex] = { id, name, email };
+    return res.status(200).json(users[userIndex]);
+})
+
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = users.find(unique => unique.id == id );
+    if(!user){
+        return res.status(404).send("404 Not Found");
+    }
+    const userIndex = users.findIndex(unique => unique.id == id);
+    users.splice(userIndex, 1);
+    return res.status(204).send();
+})
 
 // Do not touch the code below this comment
 // **************************************************************
